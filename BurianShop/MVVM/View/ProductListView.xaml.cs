44f0e88308ac40279e0c77f4f5d90d7c4dc3f45f
VadimChorrny <chorrny228@gmail.com>
2021-10-14 09:27:15 +0300

    add template for admin panel

--- BurianShop/MVVM/View/ProductListView.xaml.cs
+++ BurianShop/MVVM/View/ProductListView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BurianShop.MVVM.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace BurianShop.MVVM.View
         public ProductListView()
         {
             InitializeComponent();
+            this.DataContext = new MainViewModel();
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
